dc45741d0add40280116c8df45c5497bf3016e64
Ola Ouso <ouso.ola@gmail.com>
2020-10-21 21:15:44 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
+  let sentences = tutorials.map( x => {
+    let words = x.split(' ');
+    let caseTitle = words.map (
+      y => y[0].toUpperCase() + y.substr(1));
+
+     let join =   caseTitle.join(' ')
+
+     return join
+
+
+     });
+     return sentences
   return tutorials
 }
